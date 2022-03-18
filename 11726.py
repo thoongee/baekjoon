50d8c500eeb67773c606614aacb283b62d17fa93
thoongee <94193480+thoongee@users.noreply.github.com>
2022-03-18 23:45:39 +0900

    Add files via upload

--- 11726.py
+++ 11726.py
@@ -1,2 +1,27 @@
 import sys
 input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+cnt = [0]*1001
+cnt[1] = 1
+cnt[2] = 2
+
+for i in range(3,n+1):
+  cnt[i] = cnt[i-1]+cnt[i-2]
+
+print(cnt[n]%10007)
+
+##################################
+
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+cnt = [0,1,2]
+
+for i in range(3,n+1):
+  cnt[i].append(cnt[i-1]+cnt[i-2])
+
+print(cnt[n]%10007)
