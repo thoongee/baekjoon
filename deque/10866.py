fa6b71ecefa2fa0b88e28366d7357ea2aa99a35a
thoongee <94193480+thoongee@users.noreply.github.com>
2022-07-06 01:27:21 +0900

    Update 10866.py

--- deque/10866.py
+++ deque/10866.py
@@ -2,30 +2,40 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-mid = 10
-deque = []
-head = mid # 0~
-tail = mid
+mid = 5000
+deque = [None for i in range(10000)] # N의 범위가 1~10000
+head = mid # 시작지점 = 배열의 중간
+tail = mid # 마지막 원소의 index+1
+# print(deque)
+# print(len(deque))
 
 for _ in range(N):
 
     command = list(input().split())
 
     if command[0]== 'push_front':
-        deque.insert(head,int(command[1])) # insert(index, 값)
         head -= 1
+        # deque.insert(head,int(command[1])) # 처음에 deque를 None으로 채워두고 시작하니까, insert를 하면 그 값이 추가되어버림 (대체가 아니라)
+        deque[head] = int(command[1])
+        # print(deque)
+        # print('head',head)
 
     if command[0]== 'push_back':
-        deque.insert(tail, int(command[1]))
+        # deque.insert(tail, int(command[1]))
+        deque[tail] = int(command[1])
         tail += 1
+        # print(deque)
+        # print('tail',tail)
 
     if command[0]=='pop_front':
         if head == tail:
             print(-1)
         else:
+            print(deque[head])
+            # del deque[head] # pos만 옮겨주면 되는줄 -> 그래도 될걸? 나중에 덮어쓸거니까
             head += 1
-            print(deque[head-1])
-            del deque[head-1] # pos만 옮겨주면 되는줄
+            # print(deque)
+            # print('head',head)
 
     if command[0]=='pop_back':
         if head == tail:
@@ -33,10 +43,13 @@ for _ in range(N):
         else:
             tail -= 1
             print(deque[tail])
-            del deque[tail] # pos만 옮겨주면 되는줄
+            # print(deque)
+            # print('tail',tail)
+            # del deque[tail] # pos만 옮겨주면 되는줄
 
     if command[0]=='size':
         print(tail-head)
+        # print(deque)
 
     if command[0]=='empty':
         if head == tail:
@@ -49,11 +62,13 @@ for _ in range(N):
             print(-1)
         else:
             print(deque[head])
+            # print(deque)
 
     if command[0] == 'back':
         if head == tail:
             print(-1)
         else:
             print(deque[tail-1])
+            # print(deque)
 
 
