d1285a587fecd70456ea1d4b5fccd383c75cb160
thoongee <94193480+thoongee@users.noreply.github.com>
2022-07-06 01:27:46 +0900

    Update 9012.py

--- deque/9012.py
+++ deque/9012.py
@@ -1,30 +1,29 @@
+# 괄호 갯수가 맞으면 VPS가 아니라, 괄호가 잘 닫혀있으면 VPS이다.
+
 import sys
 input = sys.stdin.readline
 
 T = int(input())
 
-# 스택이용
-# 괄호 갯수가 맞으면 VPS인줄 알았다
-
 for i in range(T):
     stack = []
-    line = input().strip()
-    for item in line:
-        if stack:
-            if item =='(':
-                stack.append(item)
-            elif item==')':
-                if stack[-1]=='(':
+    ps_group = input().strip()
+    for ps in ps_group:
+        if stack: # stack에 무언가 있다면
+            if ps =='(':
+                stack.append(ps)
+            elif ps==')':
+                if stack[-1]=='(': # stack 제일 끝에 ( 가 있다면
                     stack.pop()
-                else:
-                    stack.append(item)
+                else: # stack 제일 끝에 )가 있다면
+                    stack.append(ps)
 
-        else:
-            stack.append(item)
+        else: # stack이 비어있다면
+            stack.append(ps)
 
     if stack: # stack에 괄호가 남아있다면
         print("NO")
     else:
         print("YES")
-            
-# sum 이용
+
+# sum 
