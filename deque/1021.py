b2993348e49739d6653ff0a6da6d9faf7c56017f
thoongee <94193480+thoongee@users.noreply.github.com>
2022-07-06 01:26:59 +0900

    Update 1021.py

--- deque/1021.py
+++ deque/1021.py
@@ -1,39 +1,33 @@
 # 문제를 이해하는데 시간이 좀 걸렸다
 # 예제 입력 2를 시행하면 내 생각엔 5가 출력인데, 왜 8인지.. --> 이 문제에서는 뒤에서는 뺄 수 없다! 앞에서만 원소를 뺄 수 있다
-# while 조건 자꾸 빼먹는데, 꼭 필요한 조건임!!
-
 # queue : FIFO
-# deque : 양방향에서 삽입, 삭제 가능
-## deque.index / deque.pop / deque.popleft / deque.append / deque.appendleft
+# deque : 양방향에서 삽입, 삭제 가능 (but 이문제에서는 뒤에서 빼는 기능은 사용하지 않음!)
 
 from collections import deque
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())    # queue의 크기 N, 뽑아내려는 수의 개수 M
-position = list(map(int, input().split())) # 뽑아내려는 수의 위치
-dq = deque([i for i in range(1, n+1)])  # deque([1, 2, 3,...,n])
-
-count = 0  
-for i in position: 
-    while True:     # 뽑을 때까지 계속 돌리기
-        #1번
-        if dq[0] == i:  # 1번 연산 : dq의 첫인덱스가 뽑아내려는 수의 위치와 같다면 뽑아내기
+n,m = map(int, input().split())
+pos = list(map(int, input().split()))
+dq = deque([i for i in range(1,n+1)])
+count = 0
+for i in pos:
+    #1번
+    while True: # 이거이거 꼭 넣기!
+        if dq[0]== i: # 덱의 첫번째 원소 == 뽑으려고 하는 수 일때,
             dq.popleft()
-            break
-        #2번
+            break # while문 탈출! 다음 입력값으로 이동
         else:
-            if dq.index(i) < len(dq)/2:  # 뽑아내려는 수의 위치 인덱스가 dq의 길이를 반으로 나눈것보다 작을때는 왼쪽으로 움직여야 최소 
-                # len(dq)대신 n 넣으면 안됌! pop 된게 있으면 길이가 달라지거든
-                
-                while dq[0] != i:   # dq의 첫번째 인덱스가 i와 같아질때까지 반복 ## == 아님!!!
+            #2번 : 뽑으려는 수가 deque의 앞부분에 있을때,
+            if dq.index(i) < len(dq)/2: # len(dq)대신 n 넣으면 안됌! pop 된게 있으면 길이가 달라지거든
+                while dq[0] !=i: # deque의 첫번째 원소 == 뽑으려고 하는 수 가 될때까지 왼쪽으로 한칸이동
                     dq.append(dq.popleft())
-                    count += 1
-            #3번
-            else:   # 뽑아내려는 수의 위치 인덱스가 dq의 길이를 반으로 나눈것보다 클때는 오른쪽으로 움직여야 최소
-                while dq[0] != i:
-                    dq.appendleft(dq.pop())
-                    count += 1
-print(count)
+                    count+=1
+            #3번 : 뽑으려는 수가 deque의 뒷부분에 있을 때,
+            else:
+                while dq[0] !=i:
+                    dq.appendleft(dq.pop()) # deque의 첫번째 원소 == 뽑으려고 하는 수 가 될때까지 오른쪽으로 한칸 이동
+                    count+=1
 
-
+sys.stdout.write(str(count)) # print보다 조금 더 빠르게 출력할 수 있으나, str형식이어야 함
+# print(count)
